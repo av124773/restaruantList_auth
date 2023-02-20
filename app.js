4400322ad22996402b023bc9890dacf001d7653a
av124773 <av124773@gmail.com>
2023-02-20 10:52:54 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// 僅在非正式機(production)環境使用dotenv
+if (process.env.MONGODB_URI !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const port = 3001
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection 
+
+db.on('error', () => {
+  console.log('mongoose error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello word')
