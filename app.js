a6e6ea94881f69107b78a193b0195c46619eb681
av124773 <av124773@gmail.com>
2023-02-21 14:00:32 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/restaurant')
 
-// 僅在非正式機(production)環境使用dotenv
 if (process.env.MONGODB_URI !== 'production') {
   require('dotenv').config()
 }
@@ -25,9 +26,21 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', {restaurant}))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
