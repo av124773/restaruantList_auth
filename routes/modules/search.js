848229f132dba93876540c0c6c9940f04c18fabc
av124773 <av124773@gmail.com>
2023-02-28 03:14:47 +0800

    feat: add search sort function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -3,10 +3,21 @@ const router = express.Router()
 
 const Restaurants = require('../../models/restaurant')
 
+const sortType = [
+  { _id: 'desc' },
+  { _id: 'asc' },
+  { name: 'asc' },
+  { name: 'desc' },
+  { category: 'asc' },
+  { location: 'asc' }
+]
+let sort = sortType[0]
+
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
   return Restaurants.find()
     .lean()
+    .sort(sort)
     .then(search => {
       const searchResult = search.filter((item) => {
         return item.name.includes(keyword) ||
@@ -15,6 +26,16 @@ router.get('/', (req, res) => {
       })
       res.render('index', { restaurants: searchResult, keyword: keyword })
     })
+    .catch(error => console.log(error))
+})
+
+router.get('/sort/:sortType', (req, res) => {
+  const index = parseInt(req.params.sortType)
+  const keyword = req.query.keyword
+  console.log(keyword)
+  sort = sortType[index]
+  return Restaurants.find()
+    .then(() => res.redirect(`/search?keyword=${keyword}`))
     .catch(error => console.log(error))
 })
 
